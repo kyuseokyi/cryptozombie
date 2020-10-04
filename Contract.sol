540bd961a2f8428e17f40fecf846344d4d877c9b
kyuseokyi <kyuseok.y@gmail.com>
2020-10-05 01:35:03 +0900

    Chapter 9

--- Contract.sol
+++ Contract.sol
@@ -11,7 +11,7 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
-    function createZombie(string _name, uint _dna) {
+    function _createZombie(string _name, uint _dna) private {
         zombies.push(Zombie(_name, _dna));
     }
 }
